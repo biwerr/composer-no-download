e0094f0805a58e37b5cf3765fbe37557b4badc6f
biwerr <rene.biwer@gmail.com>
2020-09-24 20:13:27 +0200

    Update Plugin.php
    Changed ENV to Bash valid Syntax, see #1

--- src/Plugin.php
+++ src/Plugin.php
@@ -9,7 +9,7 @@ use Composer\Plugin\PluginInterface;
 class Plugin implements PluginInterface {
 
     public function activate( Composer $composer, IOInterface $io ) {
-        if( 'apply' === getenv('COMPOSER-NO-DOWNLOAD')) {
+        if( 'apply' === getenv('COMPOSER_NO_DOWNLOAD')) {
             $installer = new Installer($io, $composer);
             $composer->getInstallationManager()->addInstaller($installer);
         }
